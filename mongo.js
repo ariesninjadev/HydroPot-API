4c4930c5e55e66fd56ac9b50a6378d6a3268c177
Powvalla <APowvalla26@jesuitmail.org>
2024-06-04 09:27:53 -0700

    6/4

--- mongo.js
+++ mongo.js
@@ -12,6 +12,7 @@ const userSchema = new mongoose.Schema(
         name: { type: String, required: true },
         pwd: { type: String, required: true },
         premium: { type: Boolean, required: true, default: false },
+        admin: { type: Boolean, required: true, default: false },
         owned: { type: Array, required: true, default: [] },
         sessions: { type: Array, required: true, default: [] },
     },
@@ -21,7 +22,7 @@ const userSchema = new mongoose.Schema(
 const dataSchema = new mongoose.Schema(
     {
         uid: { type: String, required: true },
-        pointers: { type: Array, required: true },
+        name: { type: String, required: true },
         file: { type: String, required: true },
         expires: { type: Date, required: false },
         public: { type: Boolean, required: true },
@@ -30,8 +31,18 @@ const dataSchema = new mongoose.Schema(
     { collection: "data" }
 );
 
+const pointer = new mongoose.Schema(
+    {
+        uid: { type: String, required: true },
+        address: { type: String, required: true },
+        destination: { type: String, required: true },
+    },
+    { collection: "pointers" }
+);
+
 const User = mongoose.model("users", userSchema);
 const Data = mongoose.model("data", dataSchema);
+const Pointer = mongoose.model("pointers", pointer);
 
 // Generates a unique user ID.
 function genID() {
@@ -46,16 +57,6 @@ function genID() {
     return result;
 }
 
-// Generates a unique session ID.
-function genSessionID() {
-    let result = "";
-    const characters = "0123456789ABCDEF";
-    for (let i = 0; i < 12; i++) {
-        result += characters.charAt(Math.floor(Math.random() * characters.length));
-    }
-    return result;
-}
-
 // Generates a unique session token.
 function genSessionToken() {
     let result = "";
@@ -66,30 +67,37 @@ function genSessionToken() {
     return result;
 }
 
-// Function to generate a unique session ID and token.
-function generateSession() {
-    const sessionID = genSessionID();
-    const sessionToken = genSessionToken();
-    return { sessionID, sessionToken };
+// Validates a pointer format. Unless a user is an admin, the pointer must follow the format "[custom].[username].hypot"
+function validatePointer(pointer, username) {
+    if (pointer.split(".").length !== 3) {
+        return false;
+    }
+    if (pointer.split(".")[1] !== username) {
+        return false;
+    }
+    if (pointer.split(".")[2] !== "hypot") {
+        return false;
+    }
+    return true;
 }
 
 // Function to register a property. Property is a reference to a file.
-async function registerProperty(id, sessionID, sessionToken, file, public, access_list, expiry = null) {
+async function registerProperty(id, sessionToken, name, file, public, access_list, expiry = null) {
     try {
         const user = await User.findOne({ id: id });
         if (!user) {
             return { status: false, message: "User does not exist.", code: 2 };
         }
-        const session = user.sessions.find((session) => session.sessionID === sessionID);
-        if (!session) {
+        const session = user.sessions.indexOf(sessionToken);
+        if (session === -1) {
             return { status: false, message: "Session does not exist.", code: 4 };
         }
         if (session.sessionToken !== sessionToken) {
             return { status: false, message: "Invalid session token.", code: 5 };
         }
         const data = new Data({
-            uid: id,
-            pointers: [],
+            uid: genID(),
+            name: name,
             file: file,
             expires: expiry,
             public: public,
@@ -106,7 +114,7 @@ async function registerProperty(id, sessionID, sessionToken, file, public, acces
 }
 
 // Function to associate a pointer with a property. Requires premium access.
-async function addPointer(id, sessionID, sessionToken, propertyID, pointer) {
+async function addPointer(id, sessionToken, propertyID, pointer) {
     try {
         const user = await User.findOne({ id: id });
         if (!user) {
@@ -115,19 +123,23 @@ async function addPointer(id, sessionID, sessionToken, propertyID, pointer) {
         if (!user.premium) {
             return { status: false, message: "User does not have premium access.", code: 6 };
         }
-        const session = user.sessions.find((session) => session.sessionID === sessionID);
-        if (!session) {
+        const session = user.sessions.indexOf(sessionToken);
+        if (session === -1) {
             return { status: false, message: "Session does not exist.", code: 4 };
         }
-        if (session.sessionToken !== sessionToken) {
-            return { status: false, message: "Invalid session token.", code: 5 };
-        }
-        const data = await Data.findOne({ uid: id });
+        const data = await Data.findOne({ uid: propertyID });
         if (!data) {
             return { status: false, message: "Property does not exist.", code: 7 };
         }
-        data.pointers.push(pointer);
-        await data.save();
+        if (!validatePointer(pointer, user.username) && !user.admin) {
+            return { status: false, message: "Invalid pointer format.", code: 9 };
+        }
+        const newPointer = new Pointer({
+            uid: genID(),
+            destination: propertyID,
+            address: pointer,
+        });
+        await newPointer.save();
         return { status: true, code: -1 };
     }
     catch (error) {
@@ -136,23 +148,58 @@ async function addPointer(id, sessionID, sessionToken, propertyID, pointer) {
     }
 }
 
-// Function to search for and retrieve a property by uid OR by pointer. If the property is not public, the user must have access to it.
-async function getProperty(id, sessionID, sessionToken, propertyID) {
+// Function to update the access list of a property. Requires ownership.
+async function updateAccessList(id, sessionToken, propertyID, access_list) {
     try {
         const user = await User.findOne({ id: id });
         if (!user) {
             return { status: false, message: "User does not exist.", code: 2 };
         }
-        const session = user.sessions.find((session) => session.sessionID === sessionID);
-        if (!session) {
+        const session = user.sessions.indexOf(sessionToken);
+        if (session === -1) {
             return { status: false, message: "Session does not exist.", code: 4 };
         }
-        if (session.sessionToken !== sessionToken) {
-            return { status: false, message: "Invalid session token.", code: 5 };
-        }
-        const data = await Data.findOne({ uid: id });
+        const data = await Data.findOne({ uid: propertyID });
         if (!data) {
             return { status: false, message: "Property does not exist.", code: 7 };
+        }
+        if (!data.public && !data.shared.includes(data.id)) {
+            return { status: false, message: "Property does not exist.", code: 7 };
+        }
+        if (!user.owned.includes(data.id)) {
+            return { status: false, message: "User does not own the property.", code: 8 };
+        }
+        data.shared = access_list;
+        await data.save();
+        return { status: true, code: -1 };
+    }
+    catch (error) { 
+        console.error(`Error occurred while updating access list: ${error}`);
+        return { status: false, message: "An error occurred.", code: 0 };
+    }
+}
+
+// Function to search for and retrieve a property by uid OR by pointer. If the property is not public, the user must have access to it.
+async function getProperty(id, sessionToken, searchQuery) {
+    try {
+        const user = await User.findOne({ id: id });
+        if (!user) {
+            return { status: false, message: "User does not exist.", code: 2 };
+        }
+        const session = user.sessions.indexOf(sessionToken);
+        if (session === -1) {
+            return { status: false, message: "Session does not exist.", code: 4 };
+        }
+        const data = await Data.findOne({ uid: searchQuery });
+        if (!data) {
+            const pointer = await Pointer.findOne({ address: searchQuery });
+            if (!pointer) {
+                return { status: false, message: "Property does not exist.", code: 7 };
+            }
+            const data = await Data.findOne({ uid: pointer.destination });
+            if (!data) {
+                return { status: false, message: "Property does not exist.", code: 7 };
+            }
         }
         if (data.public) {
             return { status: true, data, code: -1 };
@@ -205,7 +252,7 @@ async function loginUser(username, pwd) {
         if (user.pwd !== pwd) {
             return { status: false, message: "Incorrect password.", code: 3};
         }
-        const session = generateSession();
+        const session = genSessionToken();
         const id = user.id;
         user.sessions.push(session);
         await user.save();
@@ -218,17 +265,17 @@ async function loginUser(username, pwd) {
 }
 
 // Function to logout a user. If the user does not exist, return false. If the session ID does not exist, return false.
-async function logoutUser(id, sessionID) {
+async function logoutUser(id, sessionToken) {
     try {
         const user = await User.findOne({ id: id });
         if (!user) {
             return { status: false, message: "User does not exist.", code: 2};
         }
-        const session = user.sessions.find((session) => session.sessionID === sessionID);
-        if (!session) {
-            return { status: false, message: "Session does not exist.", code: 4};
+        const session = user.sessions.indexOf(sessionToken);
+        if (session === -1) {
+            return { status: false, message: "Session does not exist.", code: 4 };
         }
-        user.sessions = user.sessions.filter((session) => session.sessionID !== sessionID);
+        user.sessions.splice(session, 1);
         await user.save();
         return { status: true, code: -1 };
     }
@@ -239,18 +286,15 @@ async function logoutUser(id, sessionID) {
 }
 
 // Function to get a user's properties. If the user does not exist, return false. Demands a session pair to access the property.
-async function getUserData(id, sessionID, sessionToken) {
+async function getUserData(id, sessionToken) {
     try {
         const user = await User.findOne({ id: id });
         if (!user) {
             return { status: false, message: "User does not exist.", code: 2 };
         }
-        const session = user.sessions.find((session) => session.sessionID === sessionID);
-        if (!session) {
+        const session = user.sessions.indexOf(sessionToken);
+        if (session === -1) {
             return { status: false, message: "Session does not exist.", code: 4 };
-        }
-        if (session.sessionToken !== sessionToken) {
-            return { status: false, message: "Invalid session token.", code: 5 };
         }
         return { status: true, data: user.owned, code: -1 };
     }
@@ -260,9 +304,6 @@ async function getUserData(id, sessionID, sessionToken) {
     }
 }
 
-// Function to add a new data entry to the database. If the user does not exist, return false. Demands a session pair to access the property.
-
-
 
 console.log("Thread > DB Connected on MAIN");
 
@@ -270,5 +311,22 @@ module.exports = {
     registerUser,
     loginUser,
     logoutUser,
+    getUserData,
+    getProperty,
+    registerProperty,
+    addPointer,
+    updateAccessList,
 };
 
+// Error codes:
+// 0: An internal code error occurred.
+// 1: User already exists.
+// 2: User does not exist.
+// 3: Incorrect password.
+// 4: Session does not exist.
+// 5: Invalid session token. DEPRECATED.
+// 6: User does not have premium access.
+// 7: Property does not exist.
+// 8: User does not own the property.
+// 9: Invalid pointer format.
+// -1: No error occurred.
--- server.js
+++ server.js
@@ -6,6 +6,18 @@ var cors = require("cors");
 const bodyParser = require("body-parser");
 const session = require("express-session");
 
+/* 
+  Descriptions of all database functions:
+   registerUser: Registers a new user. If the user already exists, return false.
+   loginUser: Logs in a user. If the user does not exist, return false. Assigns a session ID to the user if the sign-in is successful.
+   logoutUser: Logs out a user. If the user does not exist, return false. If the session ID does not exist, return false.
+   getUserData: Gets a user's properties. If the user does not exist, return false. Demands a session pair to access the property.
+   getProperty: Searches for and retrieves a property by uid OR by pointer. If the property is not public, the user must have access to it.
+   registerProperty: Registers a property. Property is a reference to a file.
+   addPointer: Associates a pointer with a property. Requires premium access.
+   updateAccessList: Updates the access list of a property. Requires ownership.
+*/
+
 require("dotenv").config();
 
 const DB = require("./mongo");
@@ -15,33 +27,89 @@ app.set("view engine", "ejs");
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-
 // Ping the server
 app.get("/api/ping", async (req, res) => {
     res.send("pong");
 });
 
-
-
-
-
-// Create HTTPS server
-const options = {
-    key: fs.readFileSync("private.key"),
-    cert: fs.readFileSync("certificate.crt"),
-};
-
-const PORT = 4768;
-
-const server = https.createServer(options, app);
-
-// If user connects to the server on this port from HTTP, redirect them to HTTPS
-
-const http = express();
-http.get("*", (req, res) => {
-    res.redirect("https://" + req.headers.host + req.url);
+// Register a new user
+app.post("/api/register", async (req, res) => {
+    const { username, name, password } = req.body;
+    const result = await DB.registerUser(username, name, password);
+    res.send(result);
 });
+
+// Log in a user
+app.post("/api/login", async (req, res) => {
+    const { username, password } = req.body;
+    const result = await DB.loginUser(username, password);
+    res.send(result);
+});
+
+// Log out a user
+app.post("/api/logout", async (req, res) => {
+    const { id } = req.body;
+    const sessionToken = req.headers.authorization;
+    const result = await DB.logoutUser(id, sessionToken);
+    res.send(result);
+});
+
+// Get user data
+app.get("/api/user/get/:id", async (req, res) => {
+    const id = req.params.id;
+    const sessionToken = req.headers.authorization;
+    const result = await DB.getUserData(id, sessionToken);
+    res.send(result);
+});
+
+// Register a new property
+app.post("/api/property/register", async (req, res) => {
+    const { id, name, file, public, access_list, expiry } = req.body;
+    const sessionToken = req.headers.authorization;
+    const result = await DB.registerProperty(id, sessionToken, name, file, public, access_list, expiry);
+    res.send(result);
+});
+
+// Get property data. Search query is either a pointer or a property ID.
+app.get("/api/property/get/:id/:searchQuery", async (req, res) => {
+    const id = req.params.id;
+    const searchQuery = req.params.searchQuery;
+    const sessionToken = req.headers.authorization;
+    const result = await DB.getProperty(id, sessionToken, searchQuery);
+    res.send(result);
+});
+
+// Add a pointer to a property
+app.post("/api/property/pointer/add", async (req, res) => {
+    const { id, pointer, property } = req.body;
+    const sessionToken = req.headers.authorization;
+    const result = await DB.addPointer(id, sessionToken, property, pointer);
+    res.send(result);
+});
+
+// Update the access list of a property
+app.post("/api/property/access/update", async (req, res) => {
+    const { id, property, access_list } = req.body;
+    const sessionToken = req.headers.authorization;
+    const result = await DB.updateAccessList(id, sessionToken, property, access_list);
+    res.send(result);
+});
+
+let server;
+
+// Check if the key and certificate files exist
+if (fs.existsSync('private.key') && fs.existsSync('certificate.crt')) {
+    const options = {
+        key: fs.readFileSync('private.key'),
+        cert: fs.readFileSync('certificate.crt'),
+    };
+
+    server = https.createServer(options, app);
+    console.log('HTTPS Server created');
+} else {
+    server = http.createServer(app);
+    console.log('HTTP Server created');
+}
 
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
